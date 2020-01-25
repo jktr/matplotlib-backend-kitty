a53040eb81caea3349e8e187ff2620b18702bb92
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2020-01-25 17:31:28 +0100

    remove workarounds

--- __init__.py
+++ __init__.py
@@ -1,40 +1,32 @@
 # SPDX-License-Identifier: CC0-1.0
 
-import subprocess
-
-import matplotlib.backends.backend_agg as agg
-
-from matplotlib import is_interactive
 from matplotlib._pylab_helpers import Gcf
 from matplotlib.backend_bases import (
     _Backend, FigureCanvasBase, FigureManagerBase)
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+import subprocess
 
 class FigureManagerICat(FigureManagerBase):
     def show(self):
-        if not getattr(self, '_workaround', False):
-            self._workaround = True
-            return True
-
-        tmp = '/tmp/fig.png'
+        tmp = f'/tmp/fig-{self.num}.png'
         self.canvas.figure.savefig(tmp, format='png')
         subprocess.run(['kitty', '+kitten', 'icat', '--align=left', tmp])
-        
+
+
 @_Backend.export
 class _BackendICatAgg(_Backend):
-    FigureCanvas = agg.FigureCanvasAgg
+    FigureCanvas = FigureCanvasAgg
     FigureManager = FigureManagerICat
 
-    trigger_manager_draw = lambda manager: _BackendICatAgg.show()
+    def _icat_draw():
+        manager = Gcf.get_active()
+        if manager:
+            manager.show()
+        Gcf.destroy_all()
 
-    @staticmethod
-    def show(*args, block=None, **kwargs):
-        interactive = is_interactive()
-        workaround = False
-        for manager in Gcf.get_all_fig_managers():
-            workaround = workaround or manager.show()
-            if hasattr(manager, '_cidgcf'):
-                manager.canvas.mpl_disconnect(manager._cidgcf)
-            if not interactive and manager in Gcf._activeQue:
-                Gcf._activeQue.remove(manager)
-        if not workaround and interactive:
-            Gcf.destroy_all()
+    def draw_if_interactive(*args, **kwargs):
+        __class__._icat_draw()
+
+    def show(*args, **kwargs):
+        __class__._icat_draw()
