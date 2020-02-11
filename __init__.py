dff1864c5b7a0a3082467fe06c754273f1151c0a
J. Konrad Tegtmeier-Rottach <jktr@0x16.de>
2020-02-11 23:57:37 +0100

    properly scale mpl figure to kitty's size
    Note that the transparency and bg color settings used here
    make some assumptions about your terminal's color scheme.

--- __init__.py
+++ __init__.py
@@ -5,13 +5,41 @@ from matplotlib.backend_bases import (
     _Backend, FigureCanvasBase, FigureManagerBase)
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
-import subprocess
+from io import BytesIO
+from subprocess import run
 
 class FigureManagerICat(FigureManagerBase):
     def show(self):
-        tmp = f'/tmp/fig-{self.num}.png'
-        self.canvas.figure.savefig(tmp, format='png')
-        subprocess.run(['kitty', '+kitten', 'icat', '--align=left', tmp])
+
+        def run_with_output(cmd):
+            def f(args, **kwargs):
+                r = run(cmd + args, **kwargs, capture_output=True, text=True)
+                return r.stdout.rstrip()
+            return f
+
+        tput = run_with_output(['tput'])
+        icat = ['kitty', '+kitten', 'icat']
+
+        # gather terminal dimensions
+        cols = int(tput(['cols']))
+        rows = int(tput(['lines']))
+        px = run_with_output(icat)(['--print-window-size'])
+        px = list(map(int, px.split('x')))
+
+        # account for post-display prompt scrolling
+        px[1] -= int(1*(px[1]/rows))
+        rows = int(tput(['lines'])) - 1
+
+        # resize figure to terminal size
+        dpi = self.canvas.figure.dpi
+        self.canvas.figure.set_size_inches(tuple(map(lambda x: (x / dpi), px)))
+
+        with BytesIO() as buf:
+            self.canvas.figure.savefig(buf, format='png',
+                                       facecolor='#ffffff7f', transparent=True)
+
+            run(icat + ['--clear', '--place', f'{cols}x{rows}@0x0'],
+                input=buf.getbuffer())
 
 
 @_Backend.export
