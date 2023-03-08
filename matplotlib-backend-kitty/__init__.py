9eb4cbf0e56a99e3facafb54c5623c3c17a35d26
Jianhui Yuan Xu <neuromoments@gmail.com>
2023-03-08 20:31:08 +0800

    Use 'kitty icat' instead of 'kitty +kitten icat' for more compatibility

--- matplotlib-backend-kitty/__init__.py
+++ matplotlib-backend-kitty/__init__.py
@@ -32,7 +32,7 @@ class FigureManagerICat(FigureManagerBase):
 
     def show(self):
 
-        icat = __class__._run('kitty', '+kitten', 'icat')
+        icat = __class__._run('kitty', 'icat')
 
         if os.environ.get('MPLBACKEND_KITTY_SIZING', 'automatic') != 'manual':
 
